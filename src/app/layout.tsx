3316366fc70cf54b7c729d9d43f4c51e06d9a841
Sterling7718 <nora.sorence@gmail.com>
2025-03-24 03:16:40 +0800

    `Added Google Site Verification meta tag to HTML head`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="uPh6m1Ga-x4ihr9btRQjfsVvir3KUKTCA3u4MJstUco"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
